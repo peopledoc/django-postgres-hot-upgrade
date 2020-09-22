0290bf9d2bd8e672c90eeb18cad3f798c761791c
Mickaël Guérin <mickael_guerin@ultimatesoftware.com>
2020-09-22 22:39:46 +0200

    WIP Fix test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,6 +6,7 @@ def test_integration(transactional_db, settings):
     connection.connect()
     hstore = postgres_signals.get_hstore_oids("default")
     citext = postgres_signals.get_citext_oids("default")
+    defaultport = settings.DATABASES["default"]["PORT"]
     pg12port = settings.DATABASES["pg12"]["PORT"]
     # sanity check
     assert str(connection.connection.server_version).startswith("10")
@@ -15,6 +16,10 @@ def test_integration(transactional_db, settings):
     connection.connect()
 
     # Assertions
-    assert str(connection.connection.server_version).startswith("12")
-    assert postgres_signals.get_hstore_oids("default") != hstore
-    assert postgres_signals.get_citext_oids("default") != citext
+    try:
+        assert str(connection.connection.server_version).startswith("12")
+        assert postgres_signals.get_hstore_oids("default") != hstore
+        assert postgres_signals.get_citext_oids("default") != citext
+    finally:
+        settings.DATABASES["default"]["PORT"] = defaultport
+        connection.connect()
